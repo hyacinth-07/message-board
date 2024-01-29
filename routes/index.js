6ffb6cfa71fc0000549ceb947e94ef056f7e452e
hyacinth-07 <ndr.hya37@gmail.com>
2024-01-29 15:11:53 +0100

    project finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const messages = require('../messages');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Odin Mini Message Board', msg: messages });
+});
+
+router.get('/new', function (req, res, next) {
+	res.render('form');
+});
+
+router.post('/new', function (req, res, next) {
+	const text = req.body.text;
+	const user = req.body.user;
+
+	messages.push({ text: text, user: user, added: new Date() });
+	res.redirect('/');
 });
 
 module.exports = router;
